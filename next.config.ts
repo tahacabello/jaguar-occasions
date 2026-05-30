20630a4ecf6bed7b1437a2ca66ca668a284bd2c4
tahacabello <tahacabello4@gmail.com>
2026-05-30 23:50:09 +0200

    Enterprise Deploy: upload next.config.ts to root

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isGithubActions ? "/jaguar-occasions" : "",
+  assetPrefix: isGithubActions ? "/jaguar-occasions" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
