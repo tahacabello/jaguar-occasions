cec9d8c0582342598e4d212ddf82f879eb324ee3
tahacabello <tahacabello4@gmail.com>
2026-05-30 22:40:18 +0200

    Cleanup: delete old sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'jaguar-v1';
-const ASSETS = ['/', 'index.html', 'assets/css/styles.css',
-  'assets/js/app.js', 'assets/js/shared.js', 'assets/js/config.js',
-  'assets/js/supabaseClient.js', 'assets/images/default-hero.webp'];
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e =>
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
